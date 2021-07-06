9355bc9e72525405ea695f6dd989216769fef8bb
César Carabetta <carabetta87@gmail.com>
2021-07-05 23:29:54 -0300

    Mejoras

--- JS/extracciones.js
+++ JS/extracciones.js
@@ -1,46 +1,68 @@
+const cambioDinero = document.querySelector("#tipoRetiro"); 
+const retiroDinero = document.querySelector("#montoRetiro"); 
+const btnRetirar = document.querySelector("#botonRetiro");
+
+
+btnRetirar.addEventListener("click", Retiro);
+
+cambioDinero.value = "";
+retiroDinero.value = "";
+
 function tipoBillete() {
-    cambioDinero = prompt("Indique con numeros que tipos de billete desea recibir:  $100 , $200 , $500 o $1000");   
-    if(retiroDinero % cambioDinero == 0){
-        switch (cambioDinero) {
-            case "100":
-                alert(`Usted recibirá ${retiroDinero/cambioDinero} billete/s de $100, gracias por utilizar nuestros servicios`);
+      
+    if(retiroDinero.value % cambioDinero.value == 0){
+        switch (cambioDinero.value) {
+            case cambioDinero.value:                
+                retiroDiv.appendChild(retiroExitoso);                
                 break;
-            case "200":
-                alert(`Usted recibirá ${retiroDinero/cambioDinero} billete/s de $200, gracias por utilizar nuestros servicios`);
-                break;
-            case "500":
-                alert(`Usted recibirá ${retiroDinero/cambioDinero} billete/s de $500, gracias por utilizar nuestros servicios`);
-                break;
-            case "1000":
-                alert(`Usted recibirá ${retiroDinero/cambioDinero} billete/s de $1000, gracias por utilizar nuestros servicios`);
-                break; 
+            
             default: 
             alert("No ha ingresado un tipo de billete correcto");  
+            cambioDinero.value = "";
+            retiroDinero.value = "";
             }    
+            
         }else {
-            while(retiroDinero % cambioDinero != 0) {
-                alert(`El billete elegido es de mayor valor que el monto ingresado. Elija otro tipo de billete`);
+            if(retiroDinero.value % cambioDinero.value != 0) {                
+                retiroDiv.appendChild(errorRetiro);
                 tipoBillete();
         }    
+        cambioDinero.value = "";
+        retiroDinero.value = "";
     };           
   
    }
 function Retiro() {
-    retiroDinero = prompt("Ingrese el monto a retirar. Recuerde que nuestros cajeros solo permiten el retiro de cifras que sean multiplos de 100 (es decir, que terminen en 00). Monto mínimo de extracción: $100");
-    while(retiroDinero == null || retiroDinero < 100){
-        alert("Monto no ingresado o incorrecto");
-        retiroDinero = prompt("Ingrese el monto a retirar. Recuerde que nuestros cajeros solo permiten el retiro de cifras que sean multiplos de 100 (es decir, que terminen en 00). Monto mínimo de extracción: $100");
+    
+    if(retiroDinero.value == null || retiroDinero.value < 100){
+        retiroDiv.appendChild(errorRetiro);
+        //retiroDinero.value = prompt("Ingrese el monto a retirar. Recuerde que nuestros cajeros solo permiten el retiro de cifras que sean multiplos de 100 (es decir, que terminen en 00). Monto mínimo de extracción: $100");
     }
-    if(retiroDinero % 100 == 0) {    
+    if(retiroDinero.value % 100 == 0) {    
     tipoBillete();
   }
     else {
-        while(retiroDinero % 100 != 0) {
-            alert(`Debe ingresar un monto multiplo de 100 (debe terminar en 00), vuelva a intentarlo`);
+        if(retiroDinero.value % 100 != 0) {
+            
+            retiroDiv.appendChild(errorRetiro);
             Retiro();
+            cambioDinero.value = "";
+            retiroDinero.value = "";
         }
-    }
-  
+    } 
+
 }
 
-Retiro();
+const retiroDiv = document.querySelector(".extracciones");
+
+const errorRetiro = document.createElement("p");
+errorRetiro.classList.add("failRetiro");
+errorRetiro.setAttribute("id", "retiroFail");
+errorRetiro.textContent = "Monto no ingresado o incorrecto"; 
+
+const retiroExitoso = document.createElement("p");
+retiroExitoso.classList.add("okRetiro");
+retiroExitoso.setAttribute("id", "retiroSuccess");
+retiroExitoso.textContent = `Usted recibirá ${retiroDinero.value/cambioDinero.value} billete/s de ${cambioDinero.value}, gracias por utilizar nuestros servicios`; 
+
+
