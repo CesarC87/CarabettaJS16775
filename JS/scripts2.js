5f262d012ebfa5f06e45a960abfca2cb48030ed9
César Carabetta <carabetta87@gmail.com>
2021-07-20 00:40:26 -0300

    Agregados

--- JS/scripts2.js
+++ JS/scripts2.js
@@ -1,3 +1,30 @@
+
+document.querySelector("#clickAPI").addEventListener("click", getData)
+
+function getData() {
+    const xhttp = new XMLHttpRequest();    
+    xhttp.open(`GET`, `baseClientes.json`, true);
+    xhttp.send();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+         
+         let datos = JSON.parse(this.responseText);
+         let tablaJSON = document.querySelector("#tablaJSON");
+         tablaJSON.textContent = "";
+         
+         for (let item of datos){
+             console.log(item);
+             tablaJSON.innerHTML += `
+             <tr>
+                <td>${item.nombre}</td>
+                <td>${item.apellido}</td>
+             </tr>
+             `;
+         }
+        }
+      }
+  }
+
 const btnEnviar = $("#botonEnvioRegistro");
 
 const baseClientes = [
